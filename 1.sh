15a6c54959ad32d05653af04ae2922c106633551
Masyud Hossain <masyud.hossain2001@gmail.com>
2025-10-07 11:49:22 +0000

    add my first script file

---
+++ 1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+###############################
+
+#Author : Masyud Hossain
+#Date: 7/10/2025
+#Question: Number divisible by 5 & 3 but not 15
+#About: Khowing about shell
+
+#########################
+
+
+echo "Hello this is my 1st shell Script"
